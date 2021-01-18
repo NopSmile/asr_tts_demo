83d4a1dbdba15eecce1f7e4ad7193bc1f26a9c21
NOPSMILE <153223193@qq.com>
2021-01-18 10:11:42 +0800

    多线程操作

--- src/main/java/com/tx/filedown/rest/TtsController.java
+++ src/main/java/com/tx/filedown/rest/TtsController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.grpc.netty.shaded.io.netty.util.internal.StringUtil;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -27,6 +29,7 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
 
 @RestController
 @RequestMapping("/tts")
@@ -34,6 +37,8 @@ public class TtsController {
     @Value("${url}")
     private String URL;
 
+    @Resource
+    private ExecutorService executorService;
 
     @PostMapping("/go")
     public MapRestResponse save(@RequestBody Map<String,Object> map){
@@ -43,30 +48,33 @@ public class TtsController {
         String volume=(Integer)map.get("volume")==null?"0":String.valueOf((Integer)map.get("volume"));
         String text=(String)map.get("text")==null?"我是测试语音。":(String)map.get("text");
         String sid=(String)map.get("sid")==null? KeyGenerator.getUniqueID() :(String)map.get("sid");
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                final TtsParams params = new TtsParams("147258369", voiceName);
+                params.setSpeed(speed);
+                params.setVolume(volume);
 
-        final TtsParams params = new TtsParams("147258369", voiceName);
-        params.setSpeed(speed);
-        params.setVolume(volume);
-
-        final RequestBodyJson bodyJson = new RequestBodyJson();
-        bodyJson.sessionParam = convertParams(params);
-        bodyJson.text = text;
-        try {
-            final String resp = new OkHttpClient().newCall(new Request.Builder()
-                    .url(URL)
-                    .post(okhttp3.RequestBody.create(
-                            MediaType.parse("application/json; charset=utf-8"),
-                            new Gson().toJson(bodyJson)))
-                    .build())
-                    .execute()
-                    .body()
-                    .string();
-            System.out.println(resp);
-            saveResp(resp,sid);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+                final RequestBodyJson bodyJson = new RequestBodyJson();
+                bodyJson.sessionParam = convertParams(params);
+                bodyJson.text = text;
+                try {
+                    final String resp = new OkHttpClient().newCall(new Request.Builder()
+                            .url(URL)
+                            .post(okhttp3.RequestBody.create(
+                                    MediaType.parse("application/json; charset=utf-8"),
+                                    new Gson().toJson(bodyJson)))
+                            .build())
+                            .execute()
+                            .body()
+                            .string();
+                    System.out.println(resp);
+                    saveResp(resp,sid);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         String audioUrl="http://172.31.202.41:52220/tts/"+sid+".wav";
         System.out.println(audioUrl);
         //model.addAttribute("audioUrl",audioUrl);
