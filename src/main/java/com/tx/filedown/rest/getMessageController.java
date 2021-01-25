08879f46111a81de9c018fa9318c730b5fd9cc6a
NOPSMILE <153223193@qq.com>
2021-01-25 09:03:09 +0800

    修改空录音结果

--- src/main/java/com/tx/filedown/rest/getMessageController.java
+++ src/main/java/com/tx/filedown/rest/getMessageController.java
@@ -12,7 +12,7 @@ public class getMessageController {
     @GetMapping(value = "/asrmsg")
     public String selectExceptionById(@RequestParam(value="time") String timestamp) {
         if("ERROR".equals(Constant.asrmap.get(timestamp))){
-            return "未录上录音请重试！";
+            return "未听到任何录音，请重试！";
         }
         return Constant.asrmap.get(timestamp);
     }
